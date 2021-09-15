054d19a3df734d6460e71714260ba7879908c9e0
Weston Ellis <ellisweston112@gmail.com>
2021-09-15 12:00:53 -0600

    Tweak with Jamison notes

--- lib/api/repo_name.rb
+++ lib/api/repo_name.rb
@@ -1,13 +1,11 @@
 require 'faraday'
 require 'json'
 
-class RepoName
+class GithubService
 
-  def self.fetched_name
+  def self.repo_name
     response = Faraday.get 'https://api.github.com/repos/tstaros23/little-esty-shop'
     parsed = JSON.parse(response.body, symbolize_names: true) #string to hash object
     parsed[:name]
-    require "pry"; binding.pry
   end
 end
-require "pry"; binding.pry
